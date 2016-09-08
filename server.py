0ed33868ee02061fdd6f5ea98e03f3835d1d6063
MarceloHarad <marcelogdeandrade@gmail.com>
2016-09-08 15:22:29 -0300

    button classes working

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 from flask import Flask, request, redirect, url_for
 from werkzeug.utils import secure_filename
+import time
 
 
 UPLOAD_FOLDER = '/home/marcelo/Documents/Insper/Tecnologicas Web/AttendenceListFlask/static/photos'
@@ -20,57 +21,57 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
-def convertClassId(id):
+def convertClassId(id, nome):
     #Converte o ID do Semestre e Curso para uma lista de classes
     if id == 31:
-        desoft = Aula("Design de Software", 10)
+        desoft = Aula("Design de Software", 10, nome)
         db.session.add(desoft)
-        natdes = Aula("Natureza do Design", 10)
+        natdes = Aula("Natureza do Design", 10, nome)
         db.session.add(natdes)
-        modsim = Aula("Modelagem e Simulação do Mundo Físico", 10)
+        modsim = Aula("Modelagem e Simulação do Mundo Físico", 10, nome)
         db.session.add(modsim)
-        instmed = Aula("Instrumentação e Medição", 10)
+        instmed = Aula("Instrumentação e Medição", 10, nome)
         db.session.add(instmed)
-        gde = Aula("Grandes Desafios da Engenhariia", 10)
+        gde = Aula("Grandes Desafios da Engenhariia", 10, nome)
         db.session.add(gde)
         db.session.commit()
         return str(str(desoft.id) + "," + str(natdes.id) + "," + str(modsim.id) + "," +  str(instmed.id) + "," + str(gde.id))
     elif id == 32:
-        codesign = Aula("Codesign de Apps", 10)
+        codesign = Aula("Codesign de Apps", 10, nome)
         db.session.add(codesign)
-        fismov = Aula("Física do Movimento", 10)
+        fismov = Aula("Física do Movimento", 10, nome)
         db.session.add(fismov)
-        acion = Aula("Acionamentos Elétricos", 10)
+        acion = Aula("Acionamentos Elétricos", 10, nome)
         db.session.add(acion)
-        cdados = Aula("Ciência dos Dados", 10)
+        cdados = Aula("Ciência dos Dados", 10, nome)
         db.session.add(cdados)
-        matvar = Aula("Matemática da Variação", 10)
+        matvar = Aula("Matemática da Variação", 10, nome)
         db.session.add(matvar)
         db.session.commit()
         return str(str(codesign.id) + "," + str(fismov.id) + "," + str(acion.id) + "," +  str(cdados.id) + "," + str(matvar.id))
     elif id == 33:
-        desagil = Aula("Desenvolvimento Colaborativo Ágil", 10)
+        desagil = Aula("Desenvolvimento Colaborativo Ágil", 10, nome)
         db.session.add(desagil)
-        rob = Aula("Robótica Computacional", 10)
+        rob = Aula("Robótica Computacional", 10, nome)
         db.session.add(rob)
-        elesist = Aula("Elementos do Sistema", 10)
+        elesist = Aula("Elementos do Sistema", 10, nome)
         db.session.add(elesist)
-        matmult = Aula("Matemática Multivariada", 10)
+        matmult = Aula("Matemática Multivariada", 10, nome)
         db.session.add(matmult)
-        descmat = Aula("Desconstruindo a Materia", 10)
+        descmat = Aula("Desconstruindo a Materia", 10, nome)
         db.session.add(descmat)
         db.session.commit()
         return str(str(desagil.id) + "," + str(rob.id) + "," + str(elesist.id) + "," +  str(matmult.id) + "," + str(descmat.id))
     elif id == 34:
-        tecweb = Aula("Tecnologias Web", 10)
+        tecweb = Aula("Tecnologias Web", 10, nome)
         db.session.add(tecweb)
-        camfis = Aula("Camada Fisica da Computacao", 10)
+        camfis = Aula("Camada Fisica da Computacao", 10, nome)
         db.session.add(camfis)
-        eletromag = Aula("Eletromagnetismo", 10)
+        eletromag = Aula("Eletromagnetismo", 10, nome)
         db.session.add(eletromag)
-        modcom = Aula("Modelagem e Controle", 10)
+        modcom = Aula("Modelagem e Controle", 10, nome)
         db.session.add(modcom)
-        empred = Aula("Empreendedorismo Tecnologico", 10)
+        empred = Aula("Empreendedorismo Tecnologico", 10, nome)
         db.session.add(empred)
         db.session.commit()
         return str(str(tecweb.id) + "," + str(camfis.id) + "," + str(eletromag.id) + "," +  str(modcom.id) + "," + str(empred.id))
@@ -89,12 +90,14 @@ class Aula(db.Model):
     numAulas = db.Column(db.Integer(), unique=False)
     numFaltas = db.Column(db.Integer(), unique=False)
     isActive = db.Column(db.Boolean(), unique=False)
+    nameStudent = db.Column(db.String(80), unique=False)
 
-    def __init__(self,nome,numAulas):
+    def __init__(self,nome,numAulas,nameStudent):
         self.nome = nome
         self.numAulas = numAulas
         self.numFaltas = 0
         self.isActive = False
+        self.nameStudent = nameStudent
 
 
 class User(db.Model):
@@ -149,7 +152,7 @@ def root():
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], email))
 
             user = User(name = name, email= email, password= password, isProfessor = isProfessor, semesterName = semesterName, photoUrl = photoUrl)
-            user.classId = convertClassId(classId)
+            user.classId = convertClassId(classId, user.name)
             db.session.add(user)
             db.session.commit()
             return render_template('login.html')
@@ -192,7 +195,6 @@ def root():
             print("Login")
             login = request.form["email_login"]
             password = request.form["password_login"]
-            #try:
             user = User.query.filter_by(email=login).first()
             if user.password == password:
                 if not user.isProfessor:
@@ -213,14 +215,22 @@ def root():
                         Aulas.append(i)
                     print(Aulas)
                     return render_template('professor_inicial.html', aulas = Aulas, user = user)
+
+
             else:
                 print("ERRO PASSWORD")
                 #CASO SENHA DE ERRADO
                 return "Usuário não encontrado",404
-            #except:
-                #ERRO NO QUERY DO USUARIO
-                #print("ERRO TRY EXCEPT")
-                #return "Usuário não encontrado",404
+
+        elif request.form["btn"] != None:
+            print("BOTAO FUNCIONOU")
+            classe = request.form["btn"]
+            list_classes = Aula.query.filter_by(nome=classe).all()
+            email = request.form["email_professor"]
+            user = User.query.filter_by(email=email).first()
+            date = time.strftime('%d/%m/%Y')
+            return render_template('professor.html', aulas = list_classes, user = user, date = date, nome_aula = classe)
+
         else:
             return render_template('login.html')
     else:
